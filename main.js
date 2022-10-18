19ddb6a391d8d5fc1b4e67a435e22feeb0e4b857
jack mcqueen <jdmcqueen44@gmail.com>
2022-10-18 14:17:53 +0100

    Add initial functions

--- main.js
+++ main.js
@@ -1,51 +1,15 @@
-const game = (() => {
-    const board = ["X", "X", "O",
-                   "X", "O", "O",
-                   "X", "O"];
-
-    return { board, };
+const store_dom = (() => {
+    
 })();
 
-const Player = (mark) => {
-    const add_mark = () => {
+const game = (() => {
 
-    };
-    return { add_mark, };
+})();
+
+const Player = () => {
+
 };
 
-// create players and assign marks to each;
-const x = Player("X");
-const o = Player("O");
+const display = (() => {
 
-const turn_handler = ((player) => {
-    // control flow of game, pass turn from one player to another
-
-    return {  };
-})();
-
-const detect_result = (() => {
-    // detect win patterns (3 of same type in a column, row or along a diagonal) or tie (no resolution)
-    const win = () => {
-
-    };
-
-    const tie = () => {
-
-    };
-
-    return { win, tie };
-})();
-
-const announcer = (() => {
-    // announce turns, round & game wins
-
-    return {  };
-})();
-
-const restart = (() => {
-    // game over, restart game and empty contents of board array
-    const clear_board = () => {
-
-    };
-    return { clear_board, };
 })();
