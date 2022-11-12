70ca2f5c9d3c018a05ca64aa3f0fe8a30c04a7d1
jack mcqueen <jdmcqueen44@gmail.com>
2022-11-12 13:12:52 +0000

    Place computer mark automatically after user mark placement

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ const game_board = (() => {
     const fill = () => {
         for (let i = 0; i < store_dom.tiles.length; i++) {
             store_dom.tiles[i].textContent = game_board.board[i];
-        };
+        }; // computationally expensive - every time a mark is added the board the array is re-rendered to the tiles. Could plug in an argument which specifies which tile to place the mark on then add a conditional and loop for when the game needs to be restarted
     };
 
     const win_conditions = [
@@ -72,15 +72,13 @@ const game_board = (() => {
     ];
 
     const result_listener = (mark) => {
-        let search = win_conditions.find(condition => condition.every(index => board[index] === mark));
+        let search = win_conditions.find(condition => condition.every(index => board[index] === mark)); // computationally expensive - can use recursion to improve this
         if (search !== undefined) { // if true, win condition has been found
-            // highlight winning spaces
-            search.forEach(i => {
+            search.forEach(i => {// highlight winning spaces
                 store_dom.tiles[i].classList.toggle("winner"); // toggle on
             });
-            // announce winner
             mark === "X" ? display_control.result("X", "wins", "color: #fbf67f")
-                                : display_control.result("O", "wins", "color: pink");
+                                : display_control.result("O", "wins", "color: pink"); // announce winner
         } else {
             if (!board.includes("")) display_control.result("Game", "tied", "color: #60ffa5"); // prevents early tie being called
         };
@@ -94,8 +92,22 @@ const Player = (mark) => {
         if (e.target.textContent === "") {
             let data_id = Number(e.target.getAttribute("data-id")); // needs to be an integer for splicing in next step
             game_board.board.splice(data_id, 1, mark); // update board array with mark at the appropriate position
-            game_board.fill(); // render contents of array to game_board board
+            game_board.fill(); // render contents of array to board
         };
+
+        let vacancies = game_board.board.reduce((tile, value, index) => tile.concat(value === "" ? index : []), []); // set up opportunity to choose an empty index at random
+        let random = Math.floor(Math.random() * vacancies.length);
+        let index = vacancies[random];
+        setTimeout(() => {
+            if (game_board.board[index] === "") {
+                game_board.board.splice(index, 1, "O");
+                game_board.fill();
+                display_control.turn("X");
+            } else {
+                console.log("wrong");
+            }
+        }, 300);
+        // might be computationally expensive because of re-checking certain board array indexes
         game_board.result_listener(mark); // check for win/tie condition when mark is added
     };
     return { add_mark };
@@ -115,7 +127,7 @@ const game = (() => {
         } else { 
             if (e.target.textContent === "") {
                 store_dom.announcer.textContent.startsWith("X") ? display_control.turn("O") : display_control.turn("X");
-                mark === x ? mark = o : mark = x;
+                // mark === x ? mark = o : mark = x;
                 mark.add_mark(e); // ensures correct alternation of x and o - prevents "breaking" the turn via multiple clicks on the same space
             };
         };
@@ -128,3 +140,5 @@ const events = (() => {
         store_dom.tiles[i].addEventListener("click", game.turn_handler);
     };
 })();
+
+// dont need to alternate value of mark anymore
